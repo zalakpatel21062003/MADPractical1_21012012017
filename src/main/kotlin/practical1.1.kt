ef7ac979a81cc2fc63a5795f9867112af8b7d088
Denisha2001 <savaliyadenisha143@gmail.com>
2022-08-25 13:07:51 +0530

    1.2

---
+++ src/main/kotlin/practical1.1.kt
@@ -0,0 +1,20 @@
+fun main(){
+    var a:Int=100
+    val b:Float=200.95F
+    val c:Char='a'
+    val d:String="Hello World"
+    val e:Boolean=true
+    val f:Double=45.56
+    val g:Long=12345
+    val h:Short=-7
+    val i:Byte=127
+    println("Integer Value: $a")
+    println("Float Value: $b")
+    println("Character Value: $c")
+    println("String Value: $d")
+    println("Boolean Value: $e")
+    println("Double Value: $f")
+    println("Long Value: $g")
+    println("Short Value: $h")
+    println("Byte Value: $i")
+}
---
+++ src/main/kotlin/practical1.2.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var a:Int = 20
+    var b = a.toDouble()
+    var c:String = "20"
+    var d = Integer.parseInt(c)
+    var e = d.toInt()
+    var f = d.toDouble()
+    println("Integer Value: $a")
+    println("Double Value (From Integer):$b")
+    println("String Value: $c")
+    println("Integer Value1 (From String):$d")
+    println("Integer Value2 (From String):$e")
+    println("Double Value (From String):$f")
+}
